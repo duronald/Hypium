bc6599584180d538eeffec04795c9d868ba5fea0
Buslon-Brian <bbuslon@nevada.unr.edu>
2023-04-22 10:00:56 -0700

    ''Django Framework Templates&Statics"

--- niche_app/urls.py
+++ niche_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from niche_app import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path("", views.home, name="home"),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
